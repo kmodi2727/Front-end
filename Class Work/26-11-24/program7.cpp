fd6d493e41a28e5d15c897443eefb12d1cb48fa8
kmodi2727 <karanmodi26102@gmail.com>
2024-11-30 11:59:59 +0530

    Add files via upload

---
+++ Class Work/26-11-24/program7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>     // 3 variables
+main()
+{
+	int a,b;
+	printf("enter a : ");
+	scanf("%d",&a);
+	printf("enter b : ");
+	scanf("%d",&b);
+	printf("\nafter swapping a : %d",a);
+	printf("\nafter swapping b : %d",b);
+}
